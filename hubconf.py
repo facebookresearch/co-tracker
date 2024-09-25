5951295e0ac49068824f75a497ae6749379ec62b
Iurii Makarov <107990314+lvoursl@users.noreply.github.com>
2024-09-25 16:05:17 +0100

    added CoTracker2.1 via torch.hub, updated README.md (#104)

--- hubconf.py
+++ hubconf.py
@@ -7,19 +7,31 @@
 import torch
 
 _COTRACKER_URL = "https://huggingface.co/facebook/cotracker/resolve/main/cotracker2.pth"
+_COTRACKER2v1_URL = "https://huggingface.co/facebook/cotracker/resolve/main/cotracker2v1.pth"
 
 
-def _make_cotracker_predictor(*, pretrained: bool = True, online=False, **kwargs):
+def _make_cotracker_predictor(*, pretrained: bool = True, online=False, version="2", **kwargs):
     if online:
         from cotracker.predictor import CoTrackerOnlinePredictor
 
-        predictor = CoTrackerOnlinePredictor(checkpoint=None)
+        if version == "2":
+            predictor = CoTrackerOnlinePredictor(checkpoint=None)
+        elif version == "2.1":
+            predictor = CoTrackerOnlinePredictor(checkpoint=None, window_len=16)
     else:
         from cotracker.predictor import CoTrackerPredictor
 
-        predictor = CoTrackerPredictor(checkpoint=None)
+        if version == "2":
+            predictor = CoTrackerPredictor(checkpoint=None)
+        elif version == "2.1":
+            predictor = CoTrackerPredictor(checkpoint=None, window_len=16)
     if pretrained:
-        state_dict = torch.hub.load_state_dict_from_url(_COTRACKER_URL, map_location="cpu")
+        if version == "2":
+            state_dict = torch.hub.load_state_dict_from_url(_COTRACKER_URL, map_location="cpu")
+        elif version == "2.1":
+            state_dict = torch.hub.load_state_dict_from_url(_COTRACKER2v1_URL, map_location="cpu")
+        else:
+            raise Exception("Provided version does not exist")
         predictor.model.load_state_dict(state_dict)
     return predictor
 
@@ -36,3 +48,17 @@ def cotracker2_online(*, pretrained: bool = True, **kwargs):
     Online CoTracker2 with stride 4 and window length 8. Can track up to 265*265 points jointly.
     """
     return _make_cotracker_predictor(pretrained=pretrained, online=True, **kwargs)
+
+
+def cotracker2v1(*, pretrained: bool = True, **kwargs):
+    """
+    CoTracker2 with stride 4 and window length 16.
+    """
+    return _make_cotracker_predictor(pretrained=pretrained, online=False, version="2.1", **kwargs)
+
+
+def cotracker2v1_online(*, pretrained: bool = True, **kwargs):
+    """
+    Online CoTracker2 with stride 4 and window length 16.
+    """
+    return _make_cotracker_predictor(pretrained=pretrained, online=True, version="2.1", **kwargs)
